f942bf29e5ee88967414779ca85159d208a19ea2
alexsajigeorge <alexsajigeorge@gmail.com>
2024-10-03 23:47:48 +0400

    sidebar & header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import DefaultLayout from "@/components/Layout/DefaultLayout";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"></div>
+    <>
+      <DefaultLayout>
+        <h1>hello</h1>
+      </DefaultLayout>
+    </>
   );
 }
